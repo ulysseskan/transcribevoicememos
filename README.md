38adb71256e6b074c2c0d8c9e2cfb0eaebdd881f
Ulysses <71786368+ulysseskan@users.noreply.github.com>
2023-10-21 13:10:15 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# tl;dr
+Quick & dirty script to transcribe voice memos via Whisper AI
+
+# Prerequisites:
+1. You must have Whisper AI installed.  Instructions:
+
+```
+git clone https://github.com/ggerganov/whisper.cpp.git
+make
+bash ./models/download-ggml-model.sh base.en
+bash ./models/download-ggml-model.sh medium.en
+ffmpeg -i audio.m4a -ar 16000 -ac 1 -c:a pcm_s16le audio.wav
+```
+
+2. You must edit the folder names in your script and make it executable.
+
+3. Ensure all folders actually exist on your system before running.
+
+# Example usage:
+# Transcribe voice memos after 2023-10-20:
+```
+./transcribe.sh 2023-10-20
+```
+
+MIT License, feel free to modify / make better as you wish.
---
+++ transcribe.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# transcribe voice memos AFTER A CERTAIN DATE
+# via Whisper AI medium model
+
+# Check if the date parameter is provided
+if [ -z "$1" ]; then
+    echo "Error: Please provide a date parameter (./transcribe.sh 2023-10-20)"
+    exit 1
+fi
+
+# Source directory and destination directory
+source_dir="/Users/ulysses/Library/Group Containers/group.com.apple.VoiceMemos.shared/Recordings"
+destination_dir="/Users/ulysses/transcriptions"
+
+# Date parameter provided by the user
+cutoff_date="$1"
+
+# Find .m4a files modified after the cutoff date
+find "$source_dir" -name '*.m4a' -newermt "$cutoff_date" -print0 | while IFS= read -r -d $'\0' file
+do
+    # Extracting the file name without extension
+    filename=$(basename -- "$file")
+    filename_noext="${filename%.*}"
+
+    # Copy the file to the destination directory
+    cp "$file" "$destination_dir"
+
+    # Convert .m4a to .wav using ffmpeg
+    ffmpeg -i "$destination_dir/$filename" -ar 16000 -ac 1 -c:a pcm_s16le "$destination_dir/$filename_noext.wav"
+
+    # Run the whisper-c command and save output to FILENAME.txt
+    /Users/ulysses/whisper-c/main -m /Users/ulysses/whisper-c/models/ggml-medium.en.bin -f "$destination_dir/$filename_noext.wav" > "$destination_dir/$filename_noext.txt"
+
+    # Optional: Delete non .txt files from the destination directory
+    find "$destination_dir" -type f ! -name '*.txt' ! -name '*.sh' -delete
+
+done
+
+echo "Processing completed."
